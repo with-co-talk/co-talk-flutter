ba52cd9af722c2bd8d2169c4473ba8ae18ef7493
이승구 <leesg107@naver.com>
2026-01-20 23:30:04 +0900

    chore: 플랫폼별 설정 파일 추가
    - Android 플랫폼 설정 및 리소스 파일
    - iOS 플랫폼 설정 및 리소스 파일
    - macOS 플랫폼 설정 및 리소스 파일
    - Windows 플랫폼 설정 파일
    - 공통 에셋 디렉토리 구조

---
+++ android/app/src/main/kotlin/com/cotalk/co_talk_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cotalk.co_talk_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import file_selector_macos
+import flutter_secure_storage_macos
+import path_provider_foundation
+import screen_retriever_macos
+import shared_preferences_foundation
+import sqflite_darwin
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
